a722db7346177e85d5f7542310b6811bf40ae69d
Surya-Prasad06 <suryaprasadsadige007@gmail.com>
2025-02-06 17:38:17 +0530

    Initial commit

---
+++ FunctionWithSyntax.java
@@ -0,0 +1,18 @@
+// public class FunctionWithSyntax{
+// public static void FunctionName(type parameter1, type parameter2) {
+// statements or code
+// }
+//     public static void main(String[] args) {
+        // callback
+        //FunctionName(parameterValue1, parameterValue2)
+//     }
+
+// }
+
+public class FunctionWithSyntax {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ fnctionSyntax.java
@@ -0,0 +1,19 @@
+
+public class fnctionSyntax {
+    // function()
+    // syntax for function
+
+//  public static void FunctionName(){
+    // statements
+    // return
+//     }
+    public static void helloo() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String args[]) {
+        // we need to call back the function so we added this
+        // fuctionName();
+        helloo();
+    }
+}
